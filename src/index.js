cca16ce96d0f4f352c401fb6a92155d9d7a54b02
Yuriy Bevov <jury19y@mail.ru>
2021-10-14 20:51:27 +0300

    port

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 // Create port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log('Connected to port ' + PORT)
