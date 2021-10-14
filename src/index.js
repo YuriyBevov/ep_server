435b029b8e8913275ed11cb02c12ae4dd40c876f
Yuriy Bevov <jury19y@mail.ru>
2021-10-14 20:53:18 +0300

    cors

--- src/index.js
+++ src/index.js
@@ -29,16 +29,19 @@ mongoose.connect(db, {
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 
-/*app.use(function (req, res, next) {
+app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
 
   next();
-});*/
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
