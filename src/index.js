061220681942ddb12cb75180210d44ded26758fe
Yuriy Bevov <jury19y@mail.ru>
2021-10-14 15:46:56 +0300

    headers

--- src/index.js
+++ src/index.js
@@ -29,6 +29,16 @@ mongoose.connect(db, {
 
 const app = express();
 
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 app.use(cors({
     origin: true,
     credentials: true
