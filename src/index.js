babcba1ba0bcffc4654e8782c1cdf79991d72468
Yuriy Bevov <jury19y@mail.ru>
2021-10-14 20:50:32 +0300

    cors

--- src/index.js
+++ src/index.js
@@ -29,20 +29,16 @@ mongoose.connect(db, {
 
 const app = express();
 
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+app.use(cors());
 
-app.use(function (req, res, next) {
-
+/*app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
 
   next();
-});
+});*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -84,7 +80,7 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 // Create port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log('Connected to port ' + PORT)
