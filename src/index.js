b96325e5d2f0f229a380bfce439caeac8cd5454c
Yuriy Bevov <jury19y@mail.ru>
2021-10-14 14:09:07 +0300

    сменил порт

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 // Create port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log('Connected to port ' + PORT)
